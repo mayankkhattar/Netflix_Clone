8fdc13db1a23c34e0f3c64b7af5bc7b14dcbaa26
Mayank <mayankk612@gmail.com>
2025-05-04 22:12:23 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
       <Main />
       <Row rowId="1" title="UpComing" fetchUrl={requests.requestUpcoming} />
       <Row rowId="2" title="Popular" fetchUrl={requests.requestPopular} />
-      <Row rowId="3" title="Trending" fetchUrl={requests.requestTrending} />
       <Row rowId="4" title="Top Rated" fetchUrl={requests.requestTopRated} />
+      <Row rowId="3" title="Trending" fetchUrl={requests.requestTrending} />
       <Row rowId="5" title="Horror" fetchUrl={requests.requestHorror} />
     </div>
   );
